3907e854542f33d6b59f05637ea31574484463ab
Pradeep K. Pant <ppant@cpan.org>
2016-04-09 13:02:18 +0530

    Plot 4 R code and output added

---
+++ plot4.R
@@ -0,0 +1,77 @@
+## R script for producing plot 4 -- Explorartory data analysis
+## Author: Pradeep K. Pant
+## Load CRAN modules 
+library(downloader)
+library(plyr)
+library(knitr)
+library(lubridate)
+
+## Stpe 1: Download the dataset and unzip folder
+
+## Check if directory already exists?
+if(!file.exists("./projectData")){
+  dir.create("./projectData")
+}
+Url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+## Check if zip has already been downloaded in projectData directory?
+if(!file.exists("./projectData/household_power_consumption.zip")){
+  download.file(Url,destfile="./projectData/household_power_consumption.zip",mode = "wb")
+}
+## Check if zip has already been unzipped?
+if(!file.exists("./projectData/household_power_consumption.txt")){
+  unzip(zipfile="./projectData/household_power_consumption.zip",exdir="./projectData")
+}
+
+## Read the text file in R data structure 
+## We'll be reading full data 
+consumDataNew <- read.table("./projectData/household_power_consumption.txt", header =TRUE, sep = ";", stringsAsFactors=FALSE, dec=".",na.strings="?")
+
+## We need to sub set data frame based on two dates 2007-02-01 and 2007-02-02 (for 1 day)
+
+## ##DEBUG --- To do that we need to create date/time objects
+## date1 <- as.Date("01/02/2007","%d/%m/%Y")
+## time1 <- strptime("00:00:00", format = "%H:%M:%S")
+## date2 <- as.Date("02/02/2007","%d/%m/%Y")
+## time2 <- strptime("23:59:59", format = "%H:%M:%S")
+
+## Once we have data frame from the above data start plotting using base plotting system
+## Test logic 
+#subsetconsumData<-subset(consumDataNew,((consumDataNew$V1 >= date1 & consumDataNew$V2 >= time1) & (consumDataNew$V1 <= date2 & consumDataNew$V2 <= time2)
+##))
+## subSetDataOndates <- consumDataNew[consumDataNew$Date %in% c("2007-02-01","2007-02-02") ,]
+
+
+## Subsetting dates on range given in assignment
+subSetDataOndates <- consumDataNew[consumDataNew$Date %in% c("1/2/2007","2/2/2007") ,]
+## Remove full dataset
+rm(consumDataNew)
+
+## Test print
+##str(subSetDataOndates)
+
+## handling date and time .. pick from diff variables and concatenate them
+dateandtime <- strptime(paste(subSetDataOndates$Date, subSetDataOndates$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+## Make plotting parameter compatible
+globalActivePower <- as.numeric(subSetDataOndates$Global_active_power)
+globalReactivePower <- as.numeric(subSetDataOndates$Global_reactive_power)
+Voltage <- as.numeric(subSetDataOndates$Voltage)
+subMeteringData1<-as.numeric(subSetDataOndates$Sub_metering_1)
+subMeteringData2<-as.numeric(subSetDataOndates$Sub_metering_2)
+subMeteringData3<-as.numeric(subSetDataOndates$Sub_metering_3)
+
+
+## Set plot name and dimensions
+png("plot4.png", width = 480, height = 480)
+
+## Create final plots with lines type="l"
+par(mfrow = c(2, 2)) 
+## 4 plots will created in 2x2 row 
+plot(dateandtime, globalActivePower, type = "l", ylab="Global Active Power",xlab = "datetime")
+plot(dateandtime, Voltage, type = "l", ylab="Voltage", xlab = "datetime")
+plot(dateandtime, subMeteringData1, type = "l", ylab="Energy sub metering",xlab = "datetime")
+lines(dateandtime, subMeteringData2, type="l", col="red")
+lines(dateandtime, subMeteringData3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+plot(dateandtime, globalReactivePower, type = "l", ylab="Global_reactive_meter",xlab = "datetime")
+dev.off()
