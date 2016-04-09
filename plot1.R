0657a8c9308a87e10c8a7e485a1c88f2c5b40ca8
Pradeep K. Pant <ppant@cpan.org>
2016-04-09 09:25:55 +0530

    Plot 1 R code and output added

--- plot1.R
+++ plot1.R
@@ -2,8 +2,9 @@
 ## Author: Pradeep K. Pant
 ## Load CRAN modules 
 library(downloader)
-library(plyr);
+library(plyr)
 library(knitr)
+library(lubridate)
 ## Stpe 1: Download the dataset and unzip folder
 
 ## Check if directory already exists?
@@ -22,12 +23,29 @@ if(!file.exists("./projectData/household_power_consumption.txt")){
 
 ## Read the text file in R data structure 
 ## We'll be reading full data 
-consumDataNew <- read.table("./projectData/household_power_consumption.txt",header =FALSE, sep = "\t") 
+consumDataNew <- read.table("./projectData/household_power_consumption.txt", header =TRUE, sep = ";", stringsAsFactors=FALSE, dec=".")
 
 ## We need to sub set data frame based on two dates 2007-02-01 and 2007-02-02 (for 1 day)
-## To do that we need to create date/time objects
-date1 <- as.Date("2007-02-01")
-date2 <- as.Date("2007-02-02")
+## ##Test --- To do that we need to create date/time objects
+# date1 <- as.Date("01/02/2007","%d/%m/%Y")
+# time1 <- strptime("00:00:00", format = "%H:%M:%S")
+# date2 <- as.Date("02/02/2007","%d/%m/%Y")
+# time2 <- strptime("23:59:59", format = "%H:%M:%S")
 
+## Once we have data frame from the above data start plotting using base plotting system
+## Test logic 
+#subsetconsumData<-subset(consumDataNew,((consumDataNew$V1 >= date1 & consumDataNew$V2 >= time1) & (consumDataNew$V1 <= date2 & consumDataNew$V2 <= time2)
+##))
+## subSetDataOndates <- consumDataNew[consumDataNew$Date %in% c("2007-02-01","2007-02-02") ,]
 
-
+# Subsetting dates on range given in assignment
+subSetDataOndates <- consumDataNew[consumDataNew$Date %in% c("1/2/2007","2/2/2007") ,]
+# Test print
+##str(subSetDataOndates)
+# Start plooting as per assignment
+globalActivePower <- as.numeric(subSetDataOndates$Global_active_power)
+## Set plot name and dimensions
+png("plot1.png", width = 480, height = 480)
+# Create histogram
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
